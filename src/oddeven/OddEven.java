d30654e734845540d278ec0f014fd11b918e41fe
Robyn <Robyn@DESKTOP-Q53AP1R>
2016-10-04 22:06:36 -0700

    Well another week of failure I just cannot understand or get the programs that you assign to us. Every-time I try to add any extra parts to the program.I get errors and it does not come out. so this is all I have that came out at all which I know is not correct. I just do not understand what I am doing.

---
+++ src/oddeven/OddEven.java
@@ -0,0 +1,46 @@
+package oddeven;
+
+/**
+ *
+ * @author Robyn
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class OddEven {
+
+//Roll a dice to get a number between 1-6 /done
+//Ask users to guess Odd or Even /done
+/*Track and announce result:You win/lose 
+* You’ve played xx number of times, 
+* The system generate  yy  number of Odds and  zz  number of Even 
+* Your winning percentage is  nn %,
+* Print out game history as user finish playing
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random r = new Random();
+        int roll;
+        int face = 0;
+        System.out.print("Guess number either odd or even: ");
+        roll = input.nextInt();
+        int[] frequency = new int[7];//storage
+
+        //roll dice 20 times
+        for (roll = 1; roll < frequency.length; roll++) {
+            frequency[roll] = 0;
+        }
+
+        for (roll = 1; roll < 10; roll++) {
+            face = 1 + r.nextInt(6);
+        }
+        frequency[face]++;
+        System.out.printf("%s%10s%n", "Face", "Frequency");
+        //output each array's value
+        for (face = 1; face < frequency.length; face++) {
+            System.out.printf("%4d%10d%n", face, frequency[face]);
+        }
+
+    }//endmain
+}//endclass
